3f0e9bdd8bf17583fbcbd61a4f03ac7c9ed72614
chenjizhao <chenjizhao99771@163.com>
2019-01-31 11:25:07 +0800

    合并bug

--- src/views/information/consts.js
+++ src/views/information/consts.js
@@ -1,42 +1,44 @@
+import copys from '../../config/config';
 let item = {
-    prop: 'activity_type',
-    label: '激活方式',
-    width: 100,
-    isBtn: true,
-    param: 'doActive'
+  prop: 'activity_type',
+  label: '激活方式',
+  width: 100,
+  isBtn: true,
+  param: 'doActive'
 }
 let d = [{
-    prop: 'name',
-    label: '名称',
-    minWidth: 80
+  prop: 'name',
+  label: '名称',
+  minWidth: 80
 }, {
-    prop: 'create_time',
-    label: '创建时间',
-    width: 120
+  prop: 'create_time',
+  label: '创建时间',
+  width: 120
 }, {
-    prop: 'grade_id',
-    label: '学段',
-    width: 80
+  prop: 'grade_id',
+  label: '学段',
+  width: 80
 }, {
-    prop: 'subject_id',
-    label: '学科',
-    width: 80
+  prop: 'subject_id',
+  label: '学科',
+  width: 80
 },
-{
+  {
     label: '操作',
     width: 250,
     groupBtn: [{
-        text: '编辑',
-        param: 'edit'
+      text: '编辑',
+      param: 'edit'
     }, {
-        text: 'ios-trash-outline',
-        param: 'delete',
-        hoverShow: true,
-        isIcon: true
+      text: 'ios-trash-outline',
+      param: 'delete',
+      hoverShow: true,
+      isIcon: true
     }]
-}]
+  }]
 export const taskHeadData = (type) => {
-    let d2 = []
-    if(d[3] !== item) d2 = d.splice(3,0,item); 
-    return type ? d : d2
+  let d2 = copys.copy(d,[]);
+  let d3 = copys.copy(d,[]);
+  if(d2[3] !== item) d3.splice(3,0,item);
+  return type ? d2 : d3
 }
