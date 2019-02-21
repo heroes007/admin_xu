bf9b253a949c01275d75893746c2c385413d0f76
huangyuan <1394771608@qq.com>
2019-02-21 19:50:00 +0800

    2.21 端口 PM2

--- src/api/modules/config.js
+++ src/api/modules/config.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 export default axios.create({
   // baseURL: 'http://api2.laoshi123.com/',
   // baseURL: 'http://192.168.6.114:4105/met/',
-  // baseURL: 'http://api.tl100.com/met/',
-  baseURL: 'http://192.168.6.55:4105/met/' ,
+  baseURL: 'http://api.tl100.com/met/',
+  // baseURL: 'http://192.168.6.55:4105/met/' ,
   timeout: 10000,
   withCredentials: false,
   data: {  token: localStorage.getItem('token') || '' }
--- src/api/modules/dealer.js
+++ src/api/modules/dealer.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 var api = axios.create({
-    baseURL: 'http://dealer.laoshi123.com/',
+    // baseURL: 'http://dealer.laoshi123.com/',
+    baseURL: 'http://api.tl100.com/met/',
     // baseURL: 'http://192.168.1.15:4005/',
     timeout: 10000,
     withCredentials:true
