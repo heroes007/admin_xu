a6ab298d5f946e71ac20c31a9ee5658c4522715d
chenjizhao <chenjizhao99771@163.com>
2019-03-07 19:42:09 +0800

    搜索组件

--- src/views/UserManage/index.vue
+++ src/views/UserManage/index.vue
@@ -1,7 +1,8 @@
 <template>
     <Tabs value="manage">
         <TabPane label="管理列表" name="manage">
-            <screen />
+            <screen :types="4" size-title1="管理总数" :size-num1="23" btn-name="添加管理" :select1="selectList" :select2="selectList"
+                    @selectChange1="selectChange1"  @selectChange2="selectChange2" @inputChange="inputChange" @handleClick="handleClick"/>
         </TabPane>
         <TabPane label="机构列表" name="product">标签二的内容</TabPane>
         <TabPane label="导师列表" name="teacher">标签三的内容</TabPane>
@@ -14,7 +15,39 @@
     import screen from '../../components/ScreenFrame'
   export default {
     name: "index",
-    components: {screen, student}
+    components: {screen, student},
+    data(){
+      return {
+        selectList:[
+          {
+            value:'all',
+            label:'全部机构'
+          },
+          {
+            value:'zj',
+            label:'浙江医院'
+          },
+          {
+            value:'bj',
+            label:'北京医院'
+          },
+        ]
+      }
+    },
+    methods:{
+      selectChange1(val){
+        console.log(val)
+      },
+      selectChange2(val){
+        console.log(val)
+      },
+      inputChange(val){
+        console.log(val)
+      },
+      handleClick(){
+        console.log('open modal')
+      }
+    }
   }
 </script>
 
