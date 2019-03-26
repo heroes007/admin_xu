366ef3ef5abdf75856511c5d304eae5dfd730600
lesonli <307771861@qq.com>
2019-03-26 14:37:30 +0800

    bug

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "开始编译项目!"
 npm run build
 echo "开始上传静态资源到OSS!"
 distPath="./server/dist"
-ossPath="oss://jh-static-file/met_admin/dist"
+ossPath="oss://jhyl-static-file/met_admin/dist"
 ossutil64 cp $distPath $ossPath -r -u
 echo "上传结束！"
 echo "重新启动项目！"
