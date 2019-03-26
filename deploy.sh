c0fe18900873829843dab33f832c6c8837071d0b
lesonli <307771861@qq.com>
2019-03-26 14:35:27 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ echo "开始编译项目!"
 npm run build
 echo "开始上传静态资源到OSS!"
 distPath="./server/dist"
-ossPath="oss://tlw-web-static-file/met_admin/dist"
-ossutile64 cp $distPath $ossPath -r -u
+ossPath="oss://jh-static-file/met_admin/dist"
+ossutil64 cp $distPath $ossPath -r -u
 echo "上传结束！"
 echo "重新启动项目！"
 pm2 restart met_admin
