d280240f8194b32a387948c153bb1c9396f2e491
chenjizhao <chenjizhao99771@163.com>
2019-04-04 15:06:49 +0800

    4.4

--- src/components/tables.vue
+++ src/components/tables.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Table :columns="columns" :data="datas" :height="tabelHeight">
+        <Table highlight-row @on-row-click="rowClick" :columns="columns" :data="datas" :height="tabelHeight">
             <template slot-scope="{ column, row, index }" slot="operation">
                 <Switch v-if="column.isSwitch" v-model="row[column.switchKey]" size="large" @on-change="change(row)">
                     <span slot="open">启用</span>
@@ -11,14 +11,16 @@
                <Poptip v-if="column.poptip_state&&handleBtnText(t,row,column) === '查看'" width="254" placement="bottom">
                     <Button type="text">查看</Button>
                     <div class="poptip-main" slot="content">
-                      <img class="poptip-img" src="../assets/icons/mn.jpeg"/>
-                      <div class="poptip-content"><h2>王晓东</h2><p>用户ID：ur9812</p></div>
+                      < img class="poptip-img" src="../assets/icons/mn.jpeg"/>
+                      <div class="poptip-content"><h2>王晓东</h2><p>用户ID：ur9812</p ></div>
                     </div>
                </Poptip>
-              <Button type="text" v-else-if="handleBtnShow(column,row,t)" size="small"
-                      style="margin-right: 5px" @click="show(row,index,t[1])">
-                {{handleBtnText(t,row,column)}}
-              </Button>
+               <span v-else-if="handleBtnShow(column,row,t)">
+                 <Button type="text"  size="small"
+                         style="margin-right: 5px" @click="show(row,index,t[1])">
+                   {{handleBtnText(t,row,column)}}
+                 </Button>
+               </span>
             </span>
             </template>
             <template slot-scope="{ column, row, index }" slot="radio-item">
@@ -34,139 +36,159 @@
         </Table>
     </div>
 </template>
-
 <script>
-  import postData from 'src/api/postData'
-  export default {
-    data() {
-      return {
-        datas: [],
-        columns: [],
-        btnList: []
-      }
-    },
-    props: {
-      column: {
-        type: Array,
-        default: []
-      },
-      tableData: {
-        type: Array,
-        default: []
-      },
-      // isSerial -->  序号
-      isSerial: {
-        type: Boolean,
-        default: false
-      },
-      // 选项
-      isSelection: {
-        type: Boolean,
-        default: false
-      },
-      // 选项
-      isSelectionRight: {
-        type: Boolean,
-        default: false
-      },
-      //筛选选项
-      selectList: {
-        type: Array,
-      },
-      seeUrl: {
-        type: String,
-        default: ''
-      },
-      tabelHeight: {
-        type: Number
-      }
-    },
-    watch: {
-      tableData(_new) {
-        this.tableData = _new;
-        this.handleTableData(this.tableData)
-      }
-    },
-    methods: {
-      handleBtnShow(c,r,t){
-        return c.operation_btn_hide&&t[2] ? r.mark_state : true
-      },
-      radioChange(r, c) {
-        this.datas.map((t, k) => {
-          t.state = false
-          if (t.id === r.id) {
-            t.state = true;
-            this.$emit('radioChange', t)
-          }
-        })
-      },
-      handleTableData(d) {
-        d.map((t, k) => {
-          if (this.isSerial) t.serial_number = this.$config.addZero(k+1)
-          if (t.hasOwnProperty('slot')) {
-            if (t.operation.length > 0) this.btnList = t.operation
-          }
-        })
-        this.datas = d
-      },
-      show(row, rowIndex, params) {
-        if(this.seeUrl){
-           postData(this.seeUrl, {id: row.organization_id}).then((res) => {
-            if(res.data){
-              row = {...row, ...res.data[0]}
-              row.head_img_url = res.data[0].admin[0].head_img_url
-              if (this.selectList) row.list = this.getArray(this.selectList, res.data[0].admin[0])
-              this.$emit(params, row, rowIndex)
+    import postData from 'src/api/postData'
+    export default {
+        data() {
+            return {
+                datas: [],
+                columns: [],
+                btnList: []
             }
-          })
-        }else{
-          if (this.selectList) row.list = this.getArray(this.selectList, row)
-          this.$emit(params, row, rowIndex)
+        },
+        props: {
+            column: {
+                type: Array,
+                default: []
+            },
+            tableData: {
+                type: Array,
+                default: []
+            },
+            // isSerial -->  序号
+            isSerial: {
+                type: Boolean,
+                default: false
+            },
+            // 选项
+            isSelection: {
+                type: Boolean,
+                default: false
+            },
+            // 选项
+            isSelectionRight: {
+                type: Boolean,
+                default: false
+            },
+            //筛选选项
+            selectList: {
+                type: Array,
+            },
+            seeUrl: {
+                type: String,
+                default: ''
+            },
+            tabelHeight: {
+                type: Number
+            }
+        },
+        watch: {
+            tableData(_new) {
+                this.tableData = _new;
+                this.handleTableData(this.tableData)
+            }
+        },
+        methods: {
+            rowClick(row,i){
+                // row.operation_btn_show = true
+                // this.datas[i].operation_btn_show = true
+                this.$nextTick(() => {
+                    this.datas.map((t,k) => {
+                        t.operation_btn_show = false
+                    })
+                    let d = this.$config.copy(this.datas[i],{})
+                    d.operation_btn_show = true
+                    console.log(d.operation_btn_show, '1111' )
+                    this.datas.splice(i,1,d)
+                    this.$forceUpdate()
+                })
+            },
+            handleBtnShow(c,r,t){
+                if (c.hasOwnProperty('operation_btn_hide')){
+                    return c.operation_btn_hide&&t[2] ? r.mark_state : true
+                }else{
+                    if(!r.operation_btn_show&&t[0] == '查看') return true
+                    else if(r.operation_btn_show) return true
+                    else return false
+                }
+            },
+            radioChange(r, c) {
+                this.datas.map((t, k) => {
+                    t.state = false
+                    if (t.id === r.id) {
+                        t.state = true;
+                        this.$emit('radioChange', t)
+                    }
+                })
+            },
+            handleTableData(d) {
+                d.map((t, k) => {
+                    if (this.isSerial) t.serial_number = this.$config.addZero(k+1)
+                    if (t.hasOwnProperty('slot')) {
+                        if (t.operation.length > 0) this.btnList = t.operation
+                    }
+                    t.operation_btn_show = false
+                })
+                this.datas = d
+            },
+            show(row, rowIndex, params) {
+                if(this.seeUrl){
+                    postData(this.seeUrl, {id: row.organization_id}).then((res) => {
+                        if(res.data){
+                            row = {...row, ...res.data[0]}
+                            row.head_img_url = res.data[0].admin[0].head_img_url
+                            if (this.selectList) row.list = this.getArray(this.selectList, res.data[0].admin[0])
+                            this.$emit(params, row, rowIndex)
+                        }
+                    })
+                }else{
+                    if (this.selectList) row.list = this.getArray(this.selectList, row)
+                    this.$emit(params, row, rowIndex)
+                }
+            },
+            handleColumns(c) {
+                if (this.isSerial) c.unshift({title: '序号', key: 'serial_number', minWidth: 80})
+                if (this.isSelection) c.unshift({type: 'selection', width: 60, align: 'center'})
+                if (this.isSelectionRight) c.push({type: 'selection', width: 60, align: 'center'})
+                c.map((t) => {
+                    if (!t.hasOwnProperty('align')) t.align = 'center'
+                    t.tooltip = true
+                })
+                this.columns = c
+            },
+            change(row) {
+                this.$emit('table-swtich', row)
+            },
+            handleBtnText(t, r, c) {
+                // operation_state -- 处理 兑换码
+                if (Array.isArray(t[0])) {
+                    if (c.operation_state ) return r.state == 1 ? t[0][0] : t[0][1]
+                    return c.operation_state && r.use_state ? t[0][0] : c.operation_state && r.use_state === 0 ? t[0][1] : t[0]
+                } else return t[0]
+            },
+            getArray(name, string) {
+                string.role_name = this.$config.status(string.role_id)
+                let arr = [], str
+                name.forEach((item, index) => {
+                    for (var x in string) {
+                        if (x == item.title) {
+                            if (item.title == 'role_id' && x == 'role_id' && string[x] == 1) {
+                                arr.push(`${item.name}: ${string.realname}`)
+                            } else {
+                                str = item.name + ':' + ' ' + (string[x] ? string[x] : '—')
+                                arr.push(str)
+                            }
+                        }
+                    }
+                })
+                return arr
+            }
+        },
+        mounted() {
+            this.handleColumns(this.column)
+            this.handleTableData(this.tableData)
         }
-      },
-      handleColumns(c) {
-        if (this.isSerial) c.unshift({title: '序号', key: 'serial_number', minWidth: 80})
-        if (this.isSelection) c.unshift({type: 'selection', width: 60, align: 'center'})
-        if (this.isSelectionRight) c.push({type: 'selection', width: 60, align: 'center'})
-        c.map((t) => {
-          if (!t.hasOwnProperty('align')) t.align = 'center'
-          t.tooltip = true
-        })
-        this.columns = c
-      },
-      change(row) {
-        this.$emit('table-swtich', row)
-      },
-      handleBtnText(t, r, c) {
-        // operation_state -- 处理 兑换码
-        if (Array.isArray(t[0])) {
-          if (c.operation_state ) return r.state == 1 ? t[0][0] : t[0][1]
-          return c.operation_state && r.use_state ? t[0][0] : c.operation_state && r.use_state === 0 ? t[0][1] : t[0]
-        } else return t[0]
-      },
-      getArray(name, string) {
-        string.role_name = this.$config.status(string.role_id)
-        let arr = [], str
-        name.forEach((item, index) => {
-          for (var x in string) {
-            if (x == item.title) {
-              if (item.title == 'role_id' && x == 'role_id' && string[x] == 1) {
-                arr.push(`${item.name}: ${string.realname}`)
-              } else {
-                str = item.name + ':' + ' ' + (string[x] ? string[x] : '—')
-                arr.push(str)
-              }
-            }
-          }
-        })
-        return arr
-      }
-    },
-    mounted() {
-      this.handleColumns(this.column)
-      this.handleTableData(this.tableData)
     }
-  }
 </script>
 <style lang='less' scoped>
     .state-key1, .state-key-other1 {
