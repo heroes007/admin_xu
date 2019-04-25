e564ad3f76a5900c9d6488b761605397ae24a65b
chenjizhao <chenjizhao99771@163.com>
2019-04-25 14:15:34 +0800

    下载附件1

--- src/views/tutor/job-list.vue
+++ src/views/tutor/job-list.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <tutor-modal :details="details" :type="types" @preservation-success="preservationSuccess" :student-info="studentList" :show="show" @close-modal="closeModal" :title="modalTitle"/>
+        <tutor-modal :details="details" :type="types" @preservation-success="preservationSuccess"
+                     :student-info="studentList" :show="show" @close-modal="closeModal" :title="modalTitle"/>
         <screen :types="13" :selectType2="true" :select4="selectList" :title="title" @handleBack="handleBack"/>
         <div class="state">
             <Select v-model="stateValue" class="state-select" @on-change="selectChange">
@@ -22,11 +23,12 @@
         </div>
         <div class="batch">
             <div class="batch-title">选项</div>
-            <Button class="batch-download" type="primary" ghost @click="downloadAttachments">下载附件</Button>
+            <Button class="batch-download" type="primary" ghost @click="getFileName">下载附件</Button>
             <Button class="batch-read" type="primary" @click="Marking" ghost>批阅</Button>
         </div>
-        <tables :tabel-height="tableHeight" :column="column" :table-data="list" @operation1="see" @select-tables="selectTables" @on-select-all="selectTablesAll"/>
-        <page-list  :current="current" :total="total" :page-size="pageSize" @page-list="pageList"/>
+        <tables :tabel-height="tableHeight" :column="column" :table-data="list" @operation1="see"
+                @select-tables="selectTables" @on-select-all="selectTablesAll"/>
+        <page-list :current="current" :total="total" :page-size="pageSize" @page-list="pageList"/>
     </div>
 </template>
 
@@ -38,15 +40,16 @@
     import pageMixins from '../mixins/pageMixins'
     import postData from '../../api/postData'
     import tutorModal from './modal/tutor-modal'
+
     export default {
         components: {tables, screen, pageList, tutorModal},
         mixins: [pageMixins],
         data() {
             return {
                 column: [
-                    {title: '选项', type: 'selection', width: 100 },
-                    {title: '姓名', key: 'realname', minWidth: 100 },
-                    {title: '作业附件', slot: 'accessory', minWidth: 200, align: 'left' },
+                    {title: '选项', type: 'selection', width: 100},
+                    {title: '姓名', key: 'realname', minWidth: 100},
+                    {title: '作业附件', slot: 'accessory', minWidth: 200, align: 'left'},
                     {title: '提交日期', key: 'submit_time', minWidth: 180, align: 'left'},
                     {title: '批阅', key: 'state', slot: 'state-item', stateKey: 'mark_state', minWidth: 100},
                     {
@@ -60,7 +63,7 @@
                         minWidth: 100,
                         slot: 'operation',
                         align: 'left',
-                        operation: [['查看','operation1']],
+                        operation: [['查看', 'operation1']],
                     }
                 ],
                 list: [],
@@ -96,7 +99,7 @@
             }
         },
         methods: {
-            setShowModal(text,details,type){
+            setShowModal(text, details, type) {
                 this.show = true
                 this.modalTitle = text
                 this.details = details
@@ -104,10 +107,12 @@
             },
             see(val) {
                 postData('product/homework/mark_get_list', {student_homework_id: val.id}).then((res) => {
-                    if(res.res_code == 1){
+                    if (res.res_code == 1) {
                         let d = res.data.select_result
-                        if(d.length>0){
-                            d.map((t) => { t.title = res.data.homework_detail[0].title })
+                        if (d.length > 0) {
+                            d.map((t) => {
+                                t.title = res.data.homework_detail[0].title
+                            })
                         }
                         this.setShowModal('查看作业', d, 'seeTask')
                         this.studentList = res.data.student_detail
@@ -118,7 +123,10 @@
                 this.selectionList = selection
             },
             checkTitle() {
-                postData('product/homework/get_detail', {item_id: this.$route.query.id, curriculum_type: this.$route.query.curriculum_type}).then(res => {
+                postData('product/homework/get_detail', {
+                    item_id: this.$route.query.id,
+                    curriculum_type: this.$route.query.curriculum_type
+                }).then(res => {
                     let val = res.data
                     this.setShowModal('查看题目', val, 'subject')
                 })
@@ -129,17 +137,16 @@
             handleBack() {
                 this.$router.go(-1)
             },
-            Marking(){
+            Marking() {
                 let len = this.selectionList.length
-                if(len > 0) {
+                if (len > 0) {
                     let str = ''
                     this.selectionList.map((t) => {
                         str += t.mark_state
                     })
-                    if(str.includes('0')||str.includes('1'))  this.$Message.warning('已批阅的作业不能再批阅')
+                    if (str.includes('0') || str.includes('1')) this.$Message.warning('已批阅的作业不能再批阅')
                     else this.setShowModal(`批阅作业（${len}人）`, this.selectionList, 'batch')
-                }
-                else  this.$Message.warning('请选择学员');
+                } else this.$Message.warning('请选择学员');
             },
             getList() {
                 let data = {
@@ -150,21 +157,21 @@
                     mark_state: this.mark_state
                 }
                 postData('/tutor/getHomeworkByCurr', data).then(res => {
-                    if(res.res_code == 1){
-                    res.data.list.forEach(item => {
-                        let d = item.attachment_url ? JSON.parse(item.attachment_url) : []
-                        item.accessory = d && d.length>0 ? d[0].attachment_name : ''
-                        item.state = item.mark_state == 0 ? '未通过' : item.mark_state == 1 ? '已通过' : '未批阅'
-                        // item._disabled = item.mark_state === 2 ? false : true
-                        item.score = item.mark_state == 2 ? '未评' : item.score
-                    })
-                    this.list = res.data.list
-                    this.total = res.data.count
-                    this.numList[0].num = res.data.submit_count
-                    this.numList[1].num = res.data.unmark_count
-                    this.numList[2].num = res.data.unpass_count
-                    this.numList[3].num = res.data.pass_count
-                    this.numList[4].num = res.data.review_count
+                    if (res.res_code == 1) {
+                        res.data.list.forEach(item => {
+                            let d = item.attachment_url ? JSON.parse(item.attachment_url) : []
+                            item.accessory = d && d.length > 0 ? d[0].attachment_name : ''
+                            item.state = item.mark_state == 0 ? '未通过' : item.mark_state == 1 ? '已通过' : '未批阅'
+                            // item._disabled = item.mark_state === 2 ? false : true
+                            item.score = item.mark_state == 2 ? '未评' : item.score
+                        })
+                        this.list = res.data.list
+                        this.total = res.data.count
+                        this.numList[0].num = res.data.submit_count
+                        this.numList[1].num = res.data.unmark_count
+                        this.numList[2].num = res.data.unpass_count
+                        this.numList[3].num = res.data.pass_count
+                        this.numList[4].num = res.data.review_count
                     }
                 })
             },
@@ -175,62 +182,73 @@
             closeModal(val) {
                 this.show = val
             },
-            preservationSuccess(){
+            preservationSuccess() {
                 this.getList()
             },
-            accessDown(){
-                if (this.selectionList.length > 0) {
-                    let data = {
-                        dir_name: this.selectionList.length > 1 ? this.$route.query.title + " - 作业" : '',
-                        homeworks: this.arrList
-                    }
-                    console.log(data);
+            accessDown(val) {
+                let data = {
+                    folder_name: this.selectionList.length > 1 ? "作业-" + this.$route.query.title : '',
+                    children: val
                 }
-            },
-            downloadAttachments(){
-                (async () => {
-                    await this.getFileName()
-                    await this.accessDown()
-                })()
+                postData('/tutor/downloadHomework', data).then(res => {
+                    if(res.res_code == 1) {
+                        let a = document.createElement('a')
+                        a.target = '_blank'
+                        a.href = 'http://192.168.6.55:5520/' + res.data.download_url
+                        a.click()
+                    }
+                })
             },
             getFileName() {
                 if (this.selectionList.length > 0) {
-                    this.arrList= []
+                    this.arrList = []
                     this.selectionList.forEach((item, index) => {
                         postData('product/homework/mark_get_list', {student_homework_id: item.id}).then(res => {
-                            let arr = []
+                            let arrFile = []
                             res.data.select_result.forEach((item1, index1) => {
-                                if(JSON.parse(item1.attachment_url).length) {
+                                let arr = []
+                                if (JSON.parse(item1.attachment_url).length) {
                                     JSON.parse(item1.attachment_url).forEach((item2, index2) => {
-                                        if(index1 < res.data.select_result.length - 1){
+                                        if (index1 < res.data.select_result.length - 1) {
                                             arr.push({
-                                                file_name: item2.attachment_name.slice(0, item2.attachment_name.length - item2.attachment_name.split('.')[item2.attachment_name.split('.').length - 1].length - 1) + ' - 未通过（' + (index2+1) +')' + '.' + item2.attachment_name.split('.')[item2.attachment_name.split('.').length - 1],
+                                                // file_name: item1.update_time.slice(5, 10) + item2.attachment_name.slice(0, item2.attachment_name.length - item2.attachment_name.split('.')[item2.attachment_name.split('.').length - 1].length - 1) + '未通过（' + (index2 + 1) + ')' + '.' + item2.attachment_name.split('.')[item2.attachment_name.split('.').length - 1],
+                                                file_name: item2.attachment_name,
                                                 attachment_url: item2.attachment_url
                                             })
-                                        }else{
+                                        } else {
                                             arr.push({
-                                                file_name: item2.attachment_name ,
+                                                // file_name: item1.update_time.slice(5, 10) + item2.attachment_name.slice(0, item2.attachment_name.length - item2.attachment_name.split('.')[item2.attachment_name.split('.').length - 1].length - 1) + '已通过' + '.' + item2.attachment_name.split('.')[item2.attachment_name.split('.').length - 1],
+                                                file_name: item2.attachment_name,
                                                 attachment_url: item2.attachment_url
                                             })
                                         }
                                     })
                                 }
-                                if(index1 < res.data.select_result.length - 1) {
-                                    arr.push({
-                                        txt_name: '作业内容 - 未通过（' + (index1 + 1) + ')',
+                                if (index1 < res.data.select_result.length - 1) {
+                                    var homework = {
+                                        // txt_name: item1.update_time.slice(5, 10) + '作业未通过（' + (index1 + 1) + ')',
+                                        txt_name: item1.update_time,
                                         txt_content: item1.answer
-                                    })
-                                }else{
-                                    arr.push({
-                                        txt_name: '作业内容',
+                                    }
+                                } else {
+                                    var homework = {
+                                        // txt_name: item1.update_time.slice(5, 10) + '作业已通过',
+                                        txt_name: item1.update_time,
                                         txt_content: item1.answer
-                                    })
+                                    }
                                 }
+                                homework.folder_name = item1.update_time.slice(5, 7) + item1.update_time.slice(8, 10) + '作业' + (index1 == res.data.select_result.length - 1 ? '已通过' : '未通过（' + (index1 + 1) + ')' )
+                                arrFile.push({children: arr, ...homework})
                             })
                             this.arrList.push({
-                                child_name: this.selectionList[index].realname,
-                                files: arr
+                                folder_name: this.selectionList[index].realname ? this.selectionList[index].realname + index : '未命名',
+                                children: arrFile
                             })
+                            return this.arrList
+                        }).then(res => {
+                            if(index == this.selectionList.length - 1) {
+                                this.accessDown(res)
+                            }
                         })
                     })
                 } else this.$Message.warning('请选择学员')
@@ -247,85 +265,92 @@
 </script>
 
 <style lang="less" scoped>
-    .screen{
+    .screen {
         background-color: #fff;
     }
-    /deep/ .ivu-select-selection{
+
+    /deep/ .ivu-select-selection {
         border-radius: 100px;
         height: 36px;
         padding-left: 15px;
     }
-    /deep/ .ivu-select-selection{
+
+    /deep/ .ivu-select-selection {
         background: #EEEEF6;
     }
-    .state{
+
+    .state {
         background-color: #fff;
         border-top: 1px solid #F0F0F7;
         height: 80px;
         display: flex;
         align-items: center;
         position: relative;
-        .state-select{
+
+        .state-select {
             width: 120px;
             border-radius: 18px;
             margin-left: 40px;
         }
-        .state-btn{
+
+        .state-btn {
             display: flex;
             align-items: center;
             cursor: pointer;
             margin-left: 30px;
         }
-        .state-img{
+
+        .state-img {
             height: 24px;
             width: 24px;
         }
 
-        .state-check{
+        .state-check {
             margin-left: 5px;
             letter-spacing: 2px;
         }
 
-        .state-data{
+        .state-data {
             display: flex;
             position: absolute;
             right: 40px;
         }
 
-        .state-data-box{
+        .state-data-box {
             margin-left: 30px;
 
-            &-unit{
+            &-unit {
                 font-size: 14px;
                 color: #4098FF;
             }
 
-            &-title{
+            &-title {
                 font-size: 14px;
                 color: #8C9095;
             }
         }
     }
-    .batch{
+
+    .batch {
         text-align: left;
         display: flex;
         border-top: 1px solid #F0F0F7;
-        background-color:#fff;
+        background-color: #fff;
         height: 60px;
         align-items: center;
 
-        &-title{
+        &-title {
             margin-left: 36px;
             font-size: 16px
         }
 
-        &-download{
+        &-download {
             margin-left: 40px;
             width: 100px;
             height: 30px;
         }
 
-        &-read{
+        &-read {
             margin-left: 10px;
             width: 100px;
             height: 30px;
